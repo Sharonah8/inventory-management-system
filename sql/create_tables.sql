71fa69b6668c56ec01a6cf1b71a53ad10abeaf23
Sharonah8 <nyamongosharon88@gmail.com>
2025-04-30 20:50:44 +0300

    Create suppliers table

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -6,3 +6,12 @@ CREATE TABLE users (
     role VARCHAR(30) DEFAULT 'clerk',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE suppliers (
+    supplier_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    contact_person VARCHAR(100),
+    phone VARCHAR(20),
+    email VARCHAR(100),
+    address TEXT
+);
